975ea6612f9148f904a57380e8bb3ee9e78972bd
Ganesh Gaitonde <gaitonde@gmail.com>
2018-08-12 20:40:02 +0530

    added A.java

---
+++ e_voting/src/com/Sample/A.java
@@ -0,0 +1,38 @@
+class A{
+	private String name;
+	private Long rollNo;
+	private short age;
+	A(String name,Long rollNo,short age){
+		this.name=name;
+		this.rollNo = rollNo;
+		this.age = age;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return name;
+	}
+	public void getAge(){
+	
+		return age;
+		
+	}
+	public short setAge(short age){
+		this.age = age;
+	}
+	public void getRollNo(){
+		return rollNo;
+	}
+	public Long setRollNo(Long rollNo){
+		this.rollNo = rollNo;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
